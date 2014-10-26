823e578d183c936b8d245ba5c6cbd4532c4b9302
Simon Schaufelberger <simonschaufi@googlemail.com>
2014-10-26 02:07:07 +0100

    [BUGFIX] read count and download count broken
    Fixes: #52381
    Releases: master, 1.9
    Change-Id: I2bbc98165969106b266d6ffeecf11e15494a3625
    Reviewed-on: http://review.typo3.org/33535
    Reviewed-by: Simon Schaufelberger <simonschaufi@googlemail.com>
    Tested-by: Simon Schaufelberger <simonschaufi@googlemail.com>

--- pi1/class.tx_mmforum_pi1.php
+++ pi1/class.tx_mmforum_pi1.php
@@ -5500,7 +5500,7 @@ class tx_mmforum_pi1 extends tx_mmforum_base {
 		$a = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
 
 		if (@file_exists($a['file_path']) /*&& $this->getMayRead_post($a['post_id'])*/) {
-			$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tx_mmforum_attachments', 'uid='.$aUID, array('downloads' => 'downloads + 1'));
+			$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tx_mmforum_attachments', 'uid='.$aUID, array('downloads' => 'downloads + 1'), 'downloads');
 
 			header('Content-Type: '.$a['file_type']);
 			header('Content-Length: '.filesize($a['file_path']));
